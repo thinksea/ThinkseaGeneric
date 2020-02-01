a80145b8a33edf835c5cad98e49cd316747acfb5
thinksea <thinksea@163.com>
2020-02-01 22:59:42 +0800

    2020-02-01 22:59

--- Thinksea.Net.FileUploader.HttpFileUpload(Javascript)/Thinksea.Net.FileUploader.ts
+++ Thinksea.Net.FileUploader.HttpFileUpload(Javascript)/Thinksea.Net.FileUploader.ts
@@ -1,8 +1,9 @@
-﻿/// <reference path="dts/jsext-1.0.9/jsext.d.ts" />
-/**
+﻿/**
  * 封装了基于 HTTP 协议的文件上传客户端功能。（组件“Thinksea.Net.FileUploader”的 javascript 版本）
  * @version 4.0.0.0
- * @description 此组件需要谷歌的 JS 散列算法组件“CryptoJS”（版本 3.1.9-1）支持。
+ * @description 此组件依赖于下列组件
+ * 1、jsext 1.2.0 source link：https://github.com/thinksea/jsext
+ * 2、谷歌的 JS 散列算法组件“CryptoJS”（版本 3.1.9-1）支持。 source link：https://github.com/brix/crypto-js
  */
 namespace Thinksea.Net.FileUploader {
     /**
