5d3e4b1665c9a320a899ca8871cab04f22c4d1af
thinksea <thinksea@163.com>
2022-02-26 21:40:16 +0800

    2022-02-26 21:40

--- Thinksea.Net.FileUploader_AspNetCoreDemo/Program.cs
+++ Thinksea.Net.FileUploader_AspNetCoreDemo/Program.cs
@@ -1,26 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace Thinksea.Net.FileUploader_AspNetCoreDemo
+var builder = WebApplication.CreateBuilder(args);
+
+#region 向容器中添加服务。
+// Add services to the container.
+IServiceCollection services = builder.Services;
+services.AddControllers();
+services.AddRazorPages();
+
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapRazorPages();
+
+app.Run();
